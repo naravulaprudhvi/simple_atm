db06d2ed78662c7d66e664edd29f4456730382e9
naravulaprudhvi <naravulaprudhvi1224@gmail.com>
2025-03-14 11:03:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let users = [
+    { name: "PRUDHVI", cdnum: "123456789", pin: "1234" },
+    { name: "NARENDRA", cdnum: "987654321", pin: "9874" },
+    { name: " Johnson", cdnum: "11223344", pin: "1122" }
+];
+
